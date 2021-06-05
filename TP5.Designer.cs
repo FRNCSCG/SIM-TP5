0f55aebb297dfe7995c02c411162c920dab030c3
FRNCSCG <fran_s99@live.com>
2021-06-05 19:08:52 -0300

    tp5

---
+++ TP5.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class TP5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.grilla = new System.Windows.Forms.DataGridView();
+            this.clEvento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clReloj = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clDia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clRndAutoColon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clTpoLlegadasColon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clProxLlegadaColon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clRndAutoUrquiza = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clTpoLlegadasUrquiza = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clProxLlegadaUrquiza = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clEstSemaforoColon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clColaColon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clCambioSemColon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clEstSemaforoUrquiza = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clColaUrquiza = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clCambioSemUrquiza = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clRndCruce = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clTiempoCruce = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clContadorAutos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clAcumPermanencia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSimular = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grilla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grilla
+            // 
+            this.grilla.AllowUserToAddRows = false;
+            this.grilla.AllowUserToDeleteRows = false;
+            this.grilla.AllowUserToResizeRows = false;
+            this.grilla.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grilla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grilla.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clEvento,
+            this.clReloj,
+            this.clDia,
+            this.clRndAutoColon,
+            this.clTpoLlegadasColon,
+            this.clProxLlegadaColon,
+            this.clRndAutoUrquiza,
+            this.clTpoLlegadasUrquiza,
+            this.clProxLlegadaUrquiza,
+            this.clEstSemaforoColon,
+            this.clColaColon,
+            this.clCambioSemColon,
+            this.clEstSemaforoUrquiza,
+            this.clColaUrquiza,
+            this.clCambioSemUrquiza,
+            this.clRndCruce,
+            this.clTiempoCruce,
+            this.clContadorAutos,
+            this.clAcumPermanencia});
+            this.grilla.Location = new System.Drawing.Point(25, 105);
+            this.grilla.Name = "grilla";
+            this.grilla.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.grilla.RowHeadersVisible = false;
+            this.grilla.Size = new System.Drawing.Size(951, 468);
+            this.grilla.TabIndex = 0;
+            // 
+            // clEvento
+            // 
+            this.clEvento.HeaderText = "Evento";
+            this.clEvento.Name = "clEvento";
+            // 
+            // clReloj
+            // 
+            this.clReloj.HeaderText = "Reloj";
+            this.clReloj.Name = "clReloj";
+            // 
+            // clDia
+            // 
+            this.clDia.HeaderText = "Dias";
+            this.clDia.Name = "clDia";
+            // 
+            // clRndAutoColon
+            // 
+            this.clRndAutoColon.HeaderText = "Rnd auto Colon";
+            this.clRndAutoColon.Name = "clRndAutoColon";
+            // 
+            // clTpoLlegadasColon
+            // 
+            this.clTpoLlegadasColon.HeaderText = "Tiempo entre llegadas colon";
+            this.clTpoLlegadasColon.Name = "clTpoLlegadasColon";
+            // 
+            // clProxLlegadaColon
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.clProxLlegadaColon.DefaultCellStyle = dataGridViewCellStyle1;
+            this.clProxLlegadaColon.HeaderText = "Proxima llegada colon";
+            this.clProxLlegadaColon.Name = "clProxLlegadaColon";
+            // 
+            // clRndAutoUrquiza
+            // 
+            this.clRndAutoUrquiza.HeaderText = "Rnd auto Urquiza";
+            this.clRndAutoUrquiza.Name = "clRndAutoUrquiza";
+            // 
+            // clTpoLlegadasUrquiza
+            // 
+            this.clTpoLlegadasUrquiza.HeaderText = "Tiempo entre llegadas urquiza";
+            this.clTpoLlegadasUrquiza.Name = "clTpoLlegadasUrquiza";
+            // 
+            // clProxLlegadaUrquiza
+            // 
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.clProxLlegadaUrquiza.DefaultCellStyle = dataGridViewCellStyle2;
+            this.clProxLlegadaUrquiza.HeaderText = "Proxima llegada urquiza";
+            this.clProxLlegadaUrquiza.Name = "clProxLlegadaUrquiza";
+            // 
+            // clEstSemaforoColon
+            // 
+            this.clEstSemaforoColon.HeaderText = "Estado Semaforo colon";
+            this.clEstSemaforoColon.Name = "clEstSemaforoColon";
+            // 
+            // clColaColon
+            // 
+            this.clColaColon.HeaderText = "cola colon";
+            this.clColaColon.Name = "clColaColon";
+            // 
+            // clCambioSemColon
+            // 
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.clCambioSemColon.DefaultCellStyle = dataGridViewCellStyle3;
+            this.clCambioSemColon.HeaderText = "Proximo cambio sem colon";
+            this.clCambioSemColon.Name = "clCambioSemColon";
+            // 
+            // clEstSemaforoUrquiza
+            // 
+            this.clEstSemaforoUrquiza.HeaderText = "Estado Semaforo urquiza";
+            this.clEstSemaforoUrquiza.Name = "clEstSemaforoUrquiza";
+            // 
+            // clColaUrquiza
+            // 
+            this.clColaUrquiza.HeaderText = "cola urquiza";
+            this.clColaUrquiza.Name = "clColaUrquiza";
+            // 
+            // clCambioSemUrquiza
+            // 
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.clCambioSemUrquiza.DefaultCellStyle = dataGridViewCellStyle4;
+            this.clCambioSemUrquiza.HeaderText = "Proximo Cambio Sem Urquiza";
+            this.clCambioSemUrquiza.Name = "clCambioSemUrquiza";
+            // 
+            // clRndCruce
+            // 
+            this.clRndCruce.HeaderText = "Rnd cruce";
+            this.clRndCruce.Name = "clRndCruce";
+            // 
+            // clTiempoCruce
+            // 
+            this.clTiempoCruce.HeaderText = "tiempo cruce";
+            this.clTiempoCruce.Name = "clTiempoCruce";
+            // 
+            // clContadorAutos
+            // 
+            this.clContadorAutos.HeaderText = "ContadorAutos";
+            this.clContadorAutos.Name = "clContadorAutos";
+            // 
+            // clAcumPermanencia
+            // 
+            this.clAcumPermanencia.HeaderText = "Acumulador permanencia";
+            this.clAcumPermanencia.Name = "clAcumPermanencia";
+            // 
+            // btnSimular
+            // 
+            this.btnSimular.Location = new System.Drawing.Point(12, 32);
+            this.btnSimular.Name = "btnSimular";
+            this.btnSimular.Size = new System.Drawing.Size(124, 41);
+            this.btnSimular.TabIndex = 1;
+            this.btnSimular.Text = "Simular";
+            this.btnSimular.UseVisualStyleBackColor = true;
+            this.btnSimular.Click += new System.EventHandler(this.btnSimular_Click);
+            // 
+            // TP5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 585);
+            this.Controls.Add(this.btnSimular);
+            this.Controls.Add(this.grilla);
+            this.Name = "TP5";
+            this.Text = "TP3";
+            ((System.ComponentModel.ISupportInitialize)(this.grilla)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grilla;
+        private System.Windows.Forms.Button btnSimular;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clEvento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clReloj;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clDia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clRndAutoColon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clTpoLlegadasColon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clProxLlegadaColon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clRndAutoUrquiza;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clTpoLlegadasUrquiza;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clProxLlegadaUrquiza;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clEstSemaforoColon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clColaColon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clCambioSemColon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clEstSemaforoUrquiza;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clColaUrquiza;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clCambioSemUrquiza;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clRndCruce;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clTiempoCruce;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clContadorAutos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clAcumPermanencia;
+    }
+}
---
+++ TP5.cs
@@ -0,0 +1,497 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class TP5 : Form
+    {
+        public TP5()
+        {
+            InitializeComponent();
+        }
+
+
+        double reloj = 0.00;
+        double relojDia = 0.00;
+        int contDias = 0;
+
+
+
+        int idEvento = 0;
+        //Eventos
+        //0. inizializacion
+        //1. llegada auto colon
+        //2. llegada auto urquiza
+        //3. cambio color colon
+        //4. cambio color urquiza
+        //5. fin cruce auto
+        string evento;
+
+        double nextReloj;
+        int nextEvento;
+
+        Random rnd = new Random();
+
+        //estados = 0 verde, 1 amarillo, 2 rojo;
+        int estadoSemColon;
+        int estadoSemUrquiza;
+
+
+        double rndLlegadaColon;
+        double rndLlegadaUrquiza;
+        
+
+        double tiempollegada;
+        double tiempollegadaUrquiza;
+
+        double proxLlegadaColon;
+        double proxLlegadaUrquiza;
+
+        int colaColon;
+        int colaUrquiza;
+
+        double cambioColon;
+        double cambioUrquiza;
+
+        double rndCruce;
+        double tiempoCruce;
+
+        int contadorAutos = 0;
+        int acumPermanencia = 0;
+
+        int contVuelta = 0;
+        private void btnSimular_Click(object sender, EventArgs e)
+        {
+
+
+
+            //while (contDias < 100)
+            //{
+
+                DataGridViewRow fila = new DataGridViewRow();
+
+
+                //EVENTO
+                DataGridViewTextBoxCell celdaEvento = new DataGridViewTextBoxCell();
+                if (idEvento == 0)
+                {
+                    evento = "inicializacion";
+                }
+
+                idEvento = nextEvento;
+
+
+
+                celdaEvento.Value = idEvento;
+                fila.Cells.Add(celdaEvento);
+
+                //reloj
+                if (contVuelta == 0)
+                {
+                    reloj = 0;
+                }
+                else
+                {
+                    reloj = nextReloj;
+
+                    relojDia = reloj - (7200 * contDias);
+                }
+
+
+                DataGridViewTextBoxCell celdaReloj = new DataGridViewTextBoxCell();
+                celdaReloj.Value = reloj;
+                fila.Cells.Add(celdaReloj);
+
+                //Dias
+
+                if (idEvento == 0)
+                {
+                    contDias = 0;
+                }
+
+                DataGridViewTextBoxCell celdaDias = new DataGridViewTextBoxCell();
+                celdaDias.Value = contDias + 1;
+                fila.Cells.Add(celdaDias);
+
+                //-----------------LLEGADAS--------------
+
+                //rnd llegada (colon)
+                DataGridViewTextBoxCell celdaRndLlegadaColon = new DataGridViewTextBoxCell();
+                if (idEvento == 0)
+                {
+                    rndLlegadaColon = rnd.NextDouble();
+                }
+
+
+                if (idEvento == 1)
+                {
+                    rndLlegadaColon = rnd.NextDouble();
+                }
+
+
+                celdaRndLlegadaColon.Value = rndLlegadaColon;
+                fila.Cells.Add(celdaRndLlegadaColon);
+
+                //tiempo entre llegadas (colon)
+                DataGridViewTextBoxCell celdaTiempoLlegadaColon = new DataGridViewTextBoxCell();
+
+                if (idEvento == 0)
+                {
+                    tiempollegada = 2 + rndLlegadaColon * 2;
+                }
+
+                if (reloj == proxLlegadaColon)
+                {
+                    if (relojDia >= 3600)
+                    {
+                        tiempollegada = 2 + rndLlegadaColon * 2;
+                    }
+                    else
+                    {
+                        tiempollegada = 1.5 + rndLlegadaColon * 1;
+                    }
+
+                }
+
+
+                celdaTiempoLlegadaColon.Value = tiempollegada;
+                fila.Cells.Add(celdaTiempoLlegadaColon);
+
+                // proxima llegada (colon)
+                DataGridViewTextBoxCell celdaProxLlegadaColon = new DataGridViewTextBoxCell();
+
+                if (idEvento == 0)
+                {
+                    proxLlegadaColon = reloj + tiempollegada;
+                }
+
+                if (idEvento == 1)
+                {
+                    proxLlegadaColon = reloj + tiempollegada;
+                }
+
+
+
+                celdaProxLlegadaColon.Value = proxLlegadaColon;
+                fila.Cells.Add(celdaProxLlegadaColon);
+
+                nextReloj = proxLlegadaColon;
+                nextEvento = 1;
+
+                //rnd llegada (urquiza)-------------
+                DataGridViewTextBoxCell celdaRndLlegadaUrquiza = new DataGridViewTextBoxCell();
+
+                if (idEvento == 0)
+                {
+                    rndLlegadaUrquiza = rnd.NextDouble();
+                }
+
+                if (idEvento == 2)
+                {
+                    rndLlegadaUrquiza = rnd.NextDouble();
+
+                }
+
+
+                celdaRndLlegadaUrquiza.Value = rndLlegadaUrquiza;
+                fila.Cells.Add(celdaRndLlegadaUrquiza);
+
+
+                //tiempo entre llegadas (urquiza)
+                DataGridViewTextBoxCell celdaTiempoLlegadaUrquiza = new DataGridViewTextBoxCell();
+
+                if (idEvento == 0)
+                {
+                    tiempollegadaUrquiza = 2 + rndLlegadaUrquiza * 2;
+                }
+
+                if (reloj == proxLlegadaUrquiza)
+                {
+
+                    tiempollegadaUrquiza = 0 + rndLlegadaUrquiza * 2;
+
+                }
+
+
+                celdaTiempoLlegadaUrquiza.Value = tiempollegadaUrquiza;
+                fila.Cells.Add(celdaTiempoLlegadaUrquiza);
+
+
+                // proxima llegada (urquiza)
+                DataGridViewTextBoxCell celdaProxLlegadaUrquiza = new DataGridViewTextBoxCell();
+                if (idEvento == 0)
+                {
+                    proxLlegadaUrquiza = reloj + tiempollegadaUrquiza;
+                }
+
+                if (idEvento == 2)
+                {
+                    proxLlegadaUrquiza = reloj + tiempollegadaUrquiza;
+                }
+
+
+                celdaProxLlegadaUrquiza.Value = proxLlegadaUrquiza;
+                fila.Cells.Add(celdaProxLlegadaUrquiza);
+
+                if (nextReloj > proxLlegadaUrquiza)
+                {
+                    nextReloj = proxLlegadaUrquiza;
+                    nextEvento = 2;
+                }
+
+
+
+                //-----------------SEMAFOROS--------------
+
+                //estadoColon
+                DataGridViewTextBoxCell celdaEstSemColon = new DataGridViewTextBoxCell();
+
+
+                if (idEvento == 0)
+                {
+                    estadoSemColon = 0;
+                }
+
+                if (idEvento == 3)
+                {
+                    estadoSemColon = setEstadoSemaforo(estadoSemColon);
+
+                }
+
+
+                celdaEstSemColon.Value = estadoSemColon;
+                fila.Cells.Add(celdaEstSemColon);
+
+                //colaColon
+                DataGridViewTextBoxCell celdaColaColon = new DataGridViewTextBoxCell();
+                if (idEvento == 0)
+                {
+                    colaColon = 0;
+                }
+
+                if (idEvento == 1 && estadoSemColon == 2)
+                {
+                    colaColon++;
+                }
+
+
+
+                celdaColaColon.Value = colaColon;
+                fila.Cells.Add(celdaColaColon);
+
+                //cambioColon
+                DataGridViewTextBoxCell celdaCambioColon = new DataGridViewTextBoxCell();
+                if (idEvento == 0)
+                {
+                    cambioColon = 20;
+                }
+
+                if (idEvento == 3)
+                {
+                    if (estadoSemColon == 0)
+                    {
+                        cambioColon = reloj + 20;
+                    }
+
+                    if (estadoSemColon == 1)
+                    {
+                        cambioColon = reloj + 10;
+                    }
+                    if (estadoSemColon == 2)
+                    {
+                        cambioColon = reloj + 50;
+                    }
+
+                }
+
+
+                celdaCambioColon.Value = cambioColon;
+                fila.Cells.Add(celdaCambioColon);
+
+                if (nextReloj > cambioColon)
+                {
+                    nextReloj = cambioColon;
+                    nextEvento = 3;
+                }
+
+                //estadoUrquiza----------
+                DataGridViewTextBoxCell celdaEstSemUrquiza = new DataGridViewTextBoxCell();
+                if (idEvento == 0)
+                {
+                    estadoSemUrquiza = 2;
+                }
+
+                if (idEvento == 4)
+                {
+                    estadoSemUrquiza = setEstadoSemaforo(estadoSemUrquiza);
+
+                }
+
+
+
+                celdaEstSemUrquiza.Value = estadoSemUrquiza;
+                fila.Cells.Add(celdaEstSemUrquiza);
+
+                //colaUrquiza
+                DataGridViewTextBoxCell celdaColaUrquiza = new DataGridViewTextBoxCell();
+
+                if (idEvento == 0)
+                {
+                    colaUrquiza = 0;
+                }
+
+                if (idEvento == 2 && estadoSemUrquiza == 2)
+                {
+                    colaUrquiza++;
+                }
+
+
+                celdaColaUrquiza.Value = colaUrquiza;
+                fila.Cells.Add(celdaColaUrquiza);
+
+                //cambioUrquiza
+                DataGridViewTextBoxCell celdaCambioUrquiza = new DataGridViewTextBoxCell();
+
+                if (idEvento == 0)
+                {
+                    cambioUrquiza = 30;
+                }
+
+                if (idEvento == 4)
+                {
+                    if (estadoSemUrquiza == 0)
+                    {
+                        cambioUrquiza = reloj + 40;
+                    }
+
+                    if (estadoSemUrquiza == 1)
+                    {
+                        cambioUrquiza = reloj + 10;
+                    }
+                    if (estadoSemUrquiza == 2)
+                    {
+                        cambioUrquiza = reloj + 30;
+                    }
+
+                }
+
+
+
+                celdaCambioUrquiza.Value = cambioUrquiza;
+                fila.Cells.Add(celdaCambioUrquiza);
+
+                if (nextReloj > cambioUrquiza)
+                {
+                    nextReloj = cambioUrquiza;
+                    nextEvento = 4;
+                }
+
+                //-----------------CRUCE---------------------
+
+                //RndCruce
+                DataGridViewTextBoxCell celdaRndCruce = new DataGridViewTextBoxCell();
+
+                rndCruce = rnd.NextDouble();
+                celdaRndCruce.Value = rndCruce;
+                fila.Cells.Add(celdaRndCruce);
+
+                //TiempoCruce
+                DataGridViewTextBoxCell celdaTiempoCruce = new DataGridViewTextBoxCell();
+
+
+                //si colon esta verde
+                if (estadoSemColon == 0)
+                {
+                    tiempoCruce = 3 + rndCruce * 2;
+                }
+
+                //si urquiza esta verde
+                if (estadoSemUrquiza == 0)
+                {
+                    tiempoCruce = 2.5 + rndCruce * 1;
+                }
+
+
+                celdaTiempoCruce.Value = tiempoCruce;
+                fila.Cells.Add(celdaTiempoCruce);
+
+
+                //----------------------ESTADISTICAS------------------------------------
+
+                //contador autos que salen del sistema
+                DataGridViewTextBoxCell celdaContadorAutos = new DataGridViewTextBoxCell();
+                contadorAutos = 0;
+                celdaContadorAutos.Value = contadorAutos;
+                fila.Cells.Add(celdaContadorAutos);
+
+                //permanencia
+                DataGridViewTextBoxCell celdaPermanencia = new DataGridViewTextBoxCell();
+                acumPermanencia = 0;
+                celdaPermanencia.Value = acumPermanencia;
+                fila.Cells.Add(celdaPermanencia);
+
+
+                grilla.Rows.Add(fila);
+
+
+
+
+
+                contVuelta++;
+
+                if (relojDia > 7200)
+                {
+                    relojDia = 0;
+                    contDias++;
+                }
+
+
+            //}
+
+        }
+
+
+        private int setEstadoSemaforo(int estadoSem)
+        {
+            int estado = -1;
+            if (estadoSem == 0)
+            {
+                estado = 1;
+            }
+
+            if (estadoSem == 1)
+            {
+                estado = 2;
+            }
+            if (estadoSem == 2)
+            {
+                estado = 0;
+            }
+
+            return estado;
+
+
+        }
+
+
+
+
+
+    }
+    
+    
+      
+    
+    
+    
+    
+    
+}
+
